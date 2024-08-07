a99d735dd00485a4885827e0ff52ed7d94a8e311
severitech <padilladouglas6@gmail.com>
2024-08-06 23:36:58 -0400

    Corrigiendo errores

--- src/components/transaction/TransactionList.jsx
+++ src/components/transaction/TransactionList.jsx
@@ -3,14 +3,16 @@ import TransactionItem from "./TransactionItem";
 function TransactionList() {
   const { transactions } = UseGlogbalState();
   return (
-    <div className="w-full p-4 max-h-[60vh] overflow-y-auto">
+    <>
       <h3 className="text-slate-300 text-xl font-bold w-full mb-4">History</h3>
-      <ul className="space-y-2">
-        {transactions.map((transaction) => (
-          <TransactionItem transaction={transaction} key={transaction.id} />
-        ))}
-      </ul>
-    </div>
+      <div className="w-full p-4 max-h-[60vh] overflow-y-auto">
+        <ul className="space-y-2">
+          {transactions.map((transaction) => (
+            <TransactionItem transaction={transaction} key={transaction.id} />
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 export default TransactionList;
