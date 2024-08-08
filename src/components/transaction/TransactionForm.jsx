86f96b60f59456758e9a338a78a747b044cd2c4b
severitech <padilladouglas6@gmail.com>
2024-08-07 22:58:17 -0400

    Preparando para produccion

--- src/components/transaction/TransactionForm.jsx
+++ src/components/transaction/TransactionForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState,useRef } from "react"
 import {UseGlogbalState} from '../../context/GlobalState'
 
 
@@ -7,25 +7,37 @@ function TransactionForm() {
   const {addTransactions} = UseGlogbalState()
   const [description, setDescription] = useState()
   const [amount, setAmount] = useState(0)
+  const inputRef = useRef(null);
 
-
-  const onsubmit = (e)=>{
-    e.preventDefault();
+  const onsubmit = (e) => {
+    e.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+    
     const now = new Date();
     const currentDateTime = `${now.toLocaleDateString()}`;
-   
-    addTransactions({
-      id: window.crypto.randomUUID(),
-      description,
-      amount: +amount,
-      date: currentDateTime
-    })
-  }
+    // Verificar que los campos no estén vacíos
+    if (description.trim() !== '' && amount !== '') {
+      // Añadir la transacción
+      addTransactions({
+        id: window.crypto.randomUUID(),
+        description,
+        amount: +amount,
+        date: currentDateTime
+      });
+      
+      // Limpiar los campos de entrada
+      setAmount('');
+      setDescription('');
+      inputRef.current.focus();
+    } else {
+      alert('Campos vacíos');
+    }
+  };
+
   return (
     <form onSubmit={onsubmit}>
       <h2>Description</h2>
-      <input type="text" placeholder="Enter a Description" onChange={(e) => setDescription( e.target.value)} className="bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 h-full w-full"value={description}/ >
-      <input type="number" step="0.01" placeholder ="0.00" onChange={(e) => setAmount(e.target.value)}className="bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 h-full w-full"/>
+      <input  ref={inputRef} type="text" placeholder="Enter a Description" onChange={(e) => setDescription( e.target.value)} className="bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 h-full w-full"value={description}/ >
+      <input  value={amount} type="number" step="0.01" placeholder ="0.00" onChange={(e) => setAmount(e.target.value)}className="bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 h-full w-full"/>
       <button className="bg-indigo-700 text-white rounded-lg px-3 py-2 block mb-2 w-full hover:bg-indigo-500 ">Add Transaction</button>
     </form>
   )
